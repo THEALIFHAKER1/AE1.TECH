9c39e98bde95cc2072c5ff6c179711c930246df3
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-04-08 10:45:40 +0800

    clean birthday components

---
+++ src/app/(portfolio)/(pages)/dashboard/_components/time/birthday/birthday-calculate.tsx
@@ -0,0 +1,36 @@
+'use client';
+import { birthdayConfig } from '@/config/birthday';
+import { useEffect, useState } from 'react';
+
+export default function BirthdayCalculate() {
+  console.log('client');
+  const dob = new Date(birthdayConfig.date);
+  const today = new Date();
+
+  const calculateDaysUntilBirthday = () => {
+    let nextBirthday = new Date(
+      today.getFullYear(),
+      dob.getMonth(),
+      dob.getDate()
+    );
+    if (today > nextBirthday) {
+      nextBirthday.setFullYear(nextBirthday.getFullYear() + 1);
+    }
+    const diffTime = nextBirthday.getTime() - today.getTime();
+    const daysUntilBirthday = Math.ceil(diffTime / (1000 * 60 * 60 * 24));
+    return daysUntilBirthday;
+  };
+
+  const [daysUntilBirthday, setDaysUntilBirthday] = useState(
+    calculateDaysUntilBirthday()
+  );
+
+  useEffect(() => {
+    const timeUntilNextDay = 86400000 - (Number(today) % 86400000);
+    const timer = setTimeout(() => {
+      setDaysUntilBirthday(calculateDaysUntilBirthday());
+    }, timeUntilNextDay);
+    return () => clearTimeout(timer);
+  });
+  return <>{daysUntilBirthday}</>;
+}
---
+++ src/app/(portfolio)/(pages)/dashboard/_components/time/birthday/birthday.tsx
@@ -0,0 +1,25 @@
+import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
+import Animation from '../../animation';
+import { Icons } from '@/components/icons/icons';
+import BirthdayCalculate from './birthday-calculate';
+
+export default function Birthday() {
+  return (
+    <Animation>
+      <Card className='relative h-[150px] w-full bg-purple-400 text-white md:w-[150px]'>
+        <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+          <CardTitle className='text-sm font-medium'>Birthday</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <div className='text-nowrap text-lg font-bold'>
+            <BirthdayCalculate /> Days
+          </div>
+          <p className='text-xs'>Until birthday</p>
+        </CardContent>
+        <div className='absolute right-0 top-14 opacity-30 md:bottom-0'>
+          <Icons.cake size={100} />
+        </div>
+      </Card>
+    </Animation>
+  );
+}
