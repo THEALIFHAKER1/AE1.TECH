3a8f0e3f98c7dfd411f23edb92281ceb09257ae7
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-04-28 16:09:40 +0800

    finish up duolingo activity

--- src/app/(portfolio)/(pages)/dashboard/_components/duolingo/duolingo.tsx
+++ src/app/(portfolio)/(pages)/dashboard/_components/duolingo/duolingo.tsx
@@ -1,20 +1,29 @@
 import { Card, CardHeader, CardTitle, CardContent } from '@/components/ui/card';
-import React from 'react';
+import Image from 'next/image';
 import Biodata from './biodata';
 import { getUserInfo } from '@/app/api/duolingo/get-userinfo/get-userinfo';
 import Statistics from './statistics';
-
+import DuolingoImage from './duolingo-family.png';
 export default async function Duolingo() {
   const data = await getUserInfo();
 
   return (
-    <Card className=' h-full border border-input bg-[#439c01] backdrop-blur-sm backdrop-saturate-150 '>
+    <Card className='relative h-full overflow-hidden border border-input bg-[#439c01] backdrop-blur-sm backdrop-saturate-150 '>
+      <Image
+        className='absolute -bottom-12 right-0 -z-10'
+        src={DuolingoImage}
+        alt='alt'
+        width={300}
+        height={300}
+      />
       <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-        <CardTitle className='text-sm font-medium'>Duolingo Activity</CardTitle>
+        <CardTitle className='text-nowrap text-sm font-medium text-white'>
+          Duolingo Activity
+        </CardTitle>
       </CardHeader>
       <CardContent className='grid max-h-max gap-2'>
         <Biodata data={data} />
-        {/* <Statistics data={data} /> */}
+        <Statistics data={data} />
       </CardContent>
     </Card>
   );
--- src/app/(portfolio)/(pages)/dashboard/_components/duolingo/statistics.tsx
+++ src/app/(portfolio)/(pages)/dashboard/_components/duolingo/statistics.tsx
@@ -7,40 +7,40 @@ interface StatisticsProps {
 export default function Statistics({ data }: StatisticsProps) {
   console.log(data);
   return (
-    <div className='flex w-fit gap-5 rounded-xl bg-background/20 p-3'>
-      <div>
+    <div className='grid w-full grid-flow-col gap-5 rounded-xl bg-background/20 p-3'>
+      <div className='grid place-items-center text-center'>
         <Image
           src='https://d35aaqx5ub95lt.cloudfront.net/images/profile/8a6dca76019d059a81c4c7c1145aa7a4.svg'
           alt='alt'
           width={25}
           height={25}
         />
-        <p>streak</p>
-        <p>{data.streakData.currentStreak.length}</p>
-        <p>start date</p>
-        <p>{data.streakData.currentStreak.startDate}</p>
-        <p>end date</p>
-        <p>{data.streakData.currentStreak.endDate}</p>
+        <div>
+          <p>{data.streakData.currentStreak.length}</p>
+          {/* <p>Day streak</p> */}
+        </div>
       </div>
-      <div>
+      <div className='grid place-items-center text-center'>
         <Image
           src='https://d35aaqx5ub95lt.cloudfront.net/images/profile/01ce3a817dd01842581c3d18debcbc46.svg'
           alt='alt'
           width={25}
           height={25}
         />
-        <p>total xp</p>
-        <p>{data.totalXp}</p>
+        <div>
+          <p>{data.totalXp} XP</p>
+        </div>
       </div>
-      <div>
-        <p>Current league</p>
+      <div className='grid place-items-center text-center'>
         <Image
           src='https://d35aaqx5ub95lt.cloudfront.net/images/leagues/192181672ada150becd83a74a4266ae9.svg'
           alt='alt'
           width={25}
           height={25}
         />
-        <p>bronze</p>
+        <div>
+          <p>bronze</p>
+        </div>
       </div>
     </div>
   );
