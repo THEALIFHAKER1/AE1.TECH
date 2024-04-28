29244b30e1b4b188d6fe8d4d097a1a57d3489545
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-04-29 06:24:13 +0800

    add animation (i forgot)

--- src/app/(portfolio)/(pages)/dashboard/_components/duolingo/duolingo.tsx
+++ src/app/(portfolio)/(pages)/dashboard/_components/duolingo/duolingo.tsx
@@ -4,27 +4,30 @@ import Biodata from './biodata';
 import { getUserInfo } from '@/app/api/duolingo/get-userinfo/get-userinfo';
 import Statistics from './statistics';
 import DuolingoImage from '@/assets/images/duolingo/duolingo-family.png';
+import Animation from '../animation';
 
 export default async function Duolingo() {
   const data = await getUserInfo();
   return (
-    <Card className=' relative h-full overflow-hidden border border-input bg-[#58cc02] backdrop-blur-sm backdrop-saturate-150 '>
-      <Image
-        className='absolute -bottom-12 right-0 -z-10'
-        src={DuolingoImage}
-        alt='alt'
-        width={300}
-        height={300}
-      />
-      <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
-        <CardTitle className='text-nowrap text-sm font-medium text-white'>
-          Duolingo Activity
-        </CardTitle>
-      </CardHeader>
-      <CardContent className='grid max-h-max gap-2'>
-        <Biodata data={data} />
-        <Statistics data={data} />
-      </CardContent>
-    </Card>
+    <Animation>
+      <Card className=' relative h-full overflow-hidden border border-input bg-[#58cc02] backdrop-blur-sm backdrop-saturate-150 '>
+        <Image
+          className='absolute -bottom-12 right-0 -z-10'
+          src={DuolingoImage}
+          alt='alt'
+          width={300}
+          height={300}
+        />
+        <CardHeader className='flex flex-row items-center justify-between space-y-0 pb-2'>
+          <CardTitle className='text-nowrap text-sm font-medium text-white'>
+            Duolingo Activity
+          </CardTitle>
+        </CardHeader>
+        <CardContent className='grid max-h-max gap-2'>
+          <Biodata data={data} />
+          <Statistics data={data} />
+        </CardContent>
+      </Card>
+    </Animation>
   );
 }
