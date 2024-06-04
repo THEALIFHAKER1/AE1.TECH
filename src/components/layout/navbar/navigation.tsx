075a7905954f4ee851057daa9ade32f1fe67a131
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-06-05 07:43:10 +0800

    refactor navigation active to include on slug

--- src/components/layout/navbar/navigation.tsx
+++ src/components/layout/navbar/navigation.tsx
@@ -1,28 +1,34 @@
-'use client';
-import Link from 'next/link';
-import React, { useMemo } from 'react';
-import { usePathname as Pathname } from 'next/navigation';
-import { routeConfig } from '@/config/route';
+"use client";
+import Link from "next/link";
+import React, { useMemo } from "react";
+import { usePathname as Pathname } from "next/navigation";
+import { routeConfig } from "@/config/route";
 
 function Navigation() {
   const pathname = Pathname();
 
   const renderedLinks = useMemo(() => {
-    return routeConfig.map((link) => (
-      <li
-        key={link.path}
-        className={`hover:text-primary ${
-          pathname === link.path ? 'text-primary' : 'text-primary/30'
-        }`}
-      >
-        <Link href={link.path} className='hidden md:block'>
-          {link.label}
-        </Link>
-        <Link href={link.path} className='md:hidden'>
-          <link.icon />
-        </Link>
-      </li>
-    ));
+    return routeConfig.map((link) => {
+      const isActive =
+        link.path === "/"
+          ? pathname === link.path
+          : pathname.includes(link.path);
+      return (
+        <li
+          key={link.path}
+          className={`hover:text-primary ${
+            isActive ? "text-primary" : "text-primary/30"
+          }`}
+        >
+          <Link href={link.path} className="hidden md:block">
+            {link.label}
+          </Link>
+          <Link href={link.path} className="md:hidden">
+            <link.icon />
+          </Link>
+        </li>
+      );
+    });
   }, [pathname]);
 
   return renderedLinks;
