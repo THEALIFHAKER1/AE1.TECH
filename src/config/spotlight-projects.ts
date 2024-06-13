d175c2d8cf8a0b7a1d8e0e8e8d8c2342203432ea
THEALIFHAKER1 <alifhaker1@gmail.com>
2024-06-14 06:31:48 +0800

    fix image link

--- src/config/spotlight-projects.ts
+++ src/config/spotlight-projects.ts
@@ -1,39 +1,41 @@
-import { type SpotlightProjectsTypes } from '@/types';
+import { type SpotlightProjectsTypes } from "@/types";
 
 export const spotlightProjectsConfig: SpotlightProjectsTypes[] = [
   {
-    projectName: 'METEO',
-    description: 'Real-time weather app build with Next.js 14, Typescript and Shadcn UI. That gives you all the weather information you need',
-    repo: 'https://github.com/THEALIFHAKER1/METEO',
-    image: 'https://github.com/THEALIFHAKER1/METEO/blob/master/public/preview.png',
-    link: 'https://meteo.ae1.tech/',
+    projectName: "METEO",
+    description:
+      "Real-time weather app build with Next.js 14, Typescript and Shadcn UI. That gives you all the weather information you need",
+    repo: "https://github.com/THEALIFHAKER1/METEO",
+    image:
+      "https://github.com/THEALIFHAKER1/METEO/blob/master/public/spotlight.png?raw=true",
+    link: "https://meteo.ae1.tech/",
   },
   {
-    projectName: 'Project 2',
-    description: 'Description of project 2',
-    repo: 'https://github.com',
-    image: 'https://via.placeholder.com/150',
-    link: '',
+    projectName: "Project 2",
+    description: "Description of project 2",
+    repo: "https://github.com",
+    image: "https://via.placeholder.com/150",
+    link: "",
   },
   {
-    projectName: 'Project 3',
-    description: 'Description of project 3',
-    repo: 'https://github.com',
-    image: 'https://via.placeholder.com/150',
-    link: '',
+    projectName: "Project 3",
+    description: "Description of project 3",
+    repo: "https://github.com",
+    image: "https://via.placeholder.com/150",
+    link: "",
   },
   {
-    projectName: 'Project 4',
-    description: 'Description of project 4',
-    repo: 'https://github.com',
-    image: 'https://via.placeholder.com/150',
-    link: '',
+    projectName: "Project 4",
+    description: "Description of project 4",
+    repo: "https://github.com",
+    image: "https://via.placeholder.com/150",
+    link: "",
   },
   {
-    projectName: 'Project 5',
-    description: 'Description of project 5',
-    repo: 'https://github.com',
-    image: 'https://via.placeholder.com/150',
-    link: '',
+    projectName: "Project 5",
+    description: "Description of project 5",
+    repo: "https://github.com",
+    image: "https://via.placeholder.com/150",
+    link: "",
   },
 ];
